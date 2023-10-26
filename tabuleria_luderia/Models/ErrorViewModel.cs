9f0a619c782aec637ae6bb735b5a3e0fe7cdf6ba
alethevenard <114709512+alethevenard@users.noreply.github.com>
2023-10-26 09:21:10 -0300

    Adicionar arquivos de projeto.

---
+++ tabuleria_luderia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tabuleria_luderia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
