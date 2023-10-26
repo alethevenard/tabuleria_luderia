8789c20f13677f03e9aeab2d36fab925a3bbd7e2
alethevenard <114709512+alethevenard@users.noreply.github.com>
2023-10-26 10:57:59 -0300

    Configuração do Banco de dados e da tabela Loja

---
+++ tabuleria_luderia/Migrations/20231026134109_M01-AddTableLoja.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tabuleria_luderia.Migrations
+{
+    public partial class M01AddTableLoja : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lojas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeDaLoja = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<int>(type: "int", nullable: false),
+                    Logadouro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Numero = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Complemento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cep = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lojas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lojas");
+        }
+    }
+}
---
+++ tabuleria_luderia/Models/Loja.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace tabuleria_luderia.Models
+{
+    [Table("Lojas")]
+    public class Loja
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string  NomeDaLoja { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public int Telefone { get; set; }
+        
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Logadouro { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Numero { get; set; }
+
+        public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Estado { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Cep { get; set; }
+
+
+    }
+}
